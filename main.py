364e32034231eddc49ca8813ffb7abd71ca990e4
lal126594 <lal126594@iet-gibb.ch>
2021-12-03 13:13:30 +0100

    Started developing main.py, added list and supported files

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+import os
+import re
+import requests
+
+listPath="list.txt"
+supportedPath="supported.txt"
+
+def checkSupport(link):
+    supportedOpen = open(supportedPath, 'r+')
+    supportedSites = supportedOpen.readlines()
+
+    for supportedSite in supportedSites:
+        if supportedSite in link:
+            return True
+        elif supportedSite not in link:
+            return False
+        else:
+            return None
+
+
+listOpen = open(listPath, 'r+')
+urls = listOpen.readlines()
+listOpen.close()
+
+supportedCount = 0
+unsupportedCount = 0
+failToCheckCount = 0
+for url in urls:
+    supportedBool = checkSupport(url)
+    if supportedBool == True:
+        supportedCount += 1
+    elif supportedBool == False:
+        unsupportedCount += 1
+    else:
+        failToCheckCount += 1
+
